e973198181cd17ca23130ea542880f45c9a0b910
Haruto Yamazaki <tnszk22@gmail.com>
2025-09-26 01:55:58 +0900

    no message

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { GlobalAdManager } from "@/components/GlobalAdManager";
 

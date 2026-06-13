2eaec95eb26dbafec168b771a510c1c491279618
Haruto Yamazaki <tnszk22@gmail.com>
2026-06-13 12:48:27 +0900

    no message

--- frontend/components/RacePageClient.tsx
+++ frontend/components/RacePageClient.tsx
@@ -310,6 +310,7 @@ export default function RacePageClient({
             <>
                 <DisclaimerAlert />
 
+
                 {hasNarRaces && (
                     <div className="mx-2 mb-1.5 sm:mb-2">
                         <AffiliateSlot
